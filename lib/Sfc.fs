ec781745db39c4a8468dcbbfae8e50df3beedc90
todoniko <nmtodorov@gmail.com>
2022-08-08 20:20:52 +0300

    [Improve] generation of random numbers for SFC32

--- lib/Sfc.fs
+++ lib/Sfc.fs
@@ -1,6 +1,16 @@
 namespace Clamour.Sfc
 
-type Sfc32(seed1: uint32, seed2: uint32, seed3: uint32) as this =
+[<AutoOpen>]
+module Mixers =
+    let inline mix21 (a: byref<uint32>) (b: byref<uint32>) (c: byref<uint32>) (counter: byref<uint32>) : uint32 =
+        let temp = a + b + counter in
+        counter <- 1u + counter // {BARREL_SHIFT = 21, RSHIFT = 9, LSHIFT = 3}; good sets include {21,9,3},{15,8,3};
+        a <- b ^^^ (b >>> 9)
+        b <- c + (c <<< 3)
+        c <- ((c <<< 21) ||| (c >>> 11)) + temp // 11 = 32 - 21
+        temp
+
+type Sfc32(seed1: uint32, seed2: uint32, seed3: uint32) =
     let mutable a = seed1
     let mutable b = seed2
     let mutable c = seed3
@@ -8,14 +18,8 @@ type Sfc32(seed1: uint32, seed2: uint32, seed3: uint32) as this =
 
     do
         while counter <= 15u do
-            this.next ()
+            mix21 &a &b &c &counter
 
-    member _.next() : uint32 =
-        let temp = a + b + counter in
-        counter <- 1u + counter // {BARREL_SHIFT = 21, RSHIFT = 9, LSHIFT = 3}; good sets include {21,9,3},{15,8,3};
-        a <- b ^^^ (b >>> 9)
-        b <- c + (c <<< 3)
-        c <- ((c <<< 21) ||| (c >>> 11)) + temp // 11 = 32 - 21
-        temp
+    member _.next() : uint32 = mix21 &a &b &c &counter
 
     new(seed: uint64) = Sfc32(0u, uint32 (seed >>> 0), uint32 (seed >>> 32))
